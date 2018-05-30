295dfd4c178d22a9fa28323db52dbae97ad8777c
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-30 21:18:11 +0900

    g

--- scripts/Python3_6_install.sh
+++ scripts/Python3_6_install.sh
@@ -4,7 +4,7 @@ mkdir ${prefix}python3.6
 cd ${prefix}python3.6
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
 tar zxvf Python-3.6.1.tgz
-cd Python-3.6.1
+cd ${prefix}/python3.6/Python-3.6.1
 ./configure
 make
 sudo make install
