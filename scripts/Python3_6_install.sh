1f6f96f0b2ca61e1fd625f611c4636749003517a
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-30 21:01:32 +0900

    g

--- scripts/Python3_6_install.sh
+++ scripts/Python3_6_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ${prefix}python3.6
+mkdir /home/ec2-user/python3.6
 cd ${prefix}python3.6
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
 tar zxvf Python-3.6.1.tgz
