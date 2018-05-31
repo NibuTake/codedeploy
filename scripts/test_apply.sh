42a826d50d4ed91dd02bff795b9bae96a6237644
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-31 11:00:12 +0900

    h

--- scripts/test_apply.sh
+++ scripts/test_apply.sh
@@ -2,6 +2,5 @@
 ifconfig
 cat /etc/passwd|sed -e 's/:.*//g'
 prefix='/home/ec2-user/'
-mkdir ${prefix}hogeee
 sudo /usr/local/bin/pip3.6 install virtualenv
-/usr/local/bin/virtualenv -p /usr/local/bin//python3.6 encryptfile
+/usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /home/ec2-user/encryptfile
