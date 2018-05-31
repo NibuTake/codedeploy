1b2f68e98347e1ef96a537fa25006ca7de2be86c
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-31 12:47:57 +0900

    j

---
+++ scripts/sudo_install_1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo yum -y groupinstall 'Development tools'
+sudo yum -y install zlib-devel openssl-devel
--- scripts/test_apply.sh
+++ scripts/test_apply.sh
@@ -18,4 +18,7 @@ sudo make install
 sudo /usr/local/bin/pip3.6 install virtualenv
 /usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /usr/tmp/test_encry
 
-/usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /home/ec2-user/test_encry
+cd ${prefix}test_encry
+source bin/activate
+
+pip install pandas
