08770f898bc30311c69e531f3768b8bece0cc870
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-31 12:18:03 +0900

    h

--- scripts/test_apply.sh
+++ scripts/test_apply.sh
@@ -16,7 +16,6 @@ make
 sudo make install
 
 sudo /usr/local/bin/pip3.6 install virtualenv
-/usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /usr/tmp/encryptfile
-
+/usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /usr/tmp/test_encry
 
 /usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /home/ec2-user/test_encry
