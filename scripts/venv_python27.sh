3cfaa797f1bac5a45f0e8e4768927a6f70acf9be
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-31 13:27:15 +0900

    h

--- scripts/venv_python27.sh
+++ scripts/venv_python27.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 prefix='/home/ec2-user/'
 python27path='/usr/lib64/python2.7'
 
-/usr/local/bin/virtualenv -p ${python27path} ${prefix}test27
+/usr/local/bin/virtualenv -p ${python27path} /usr/tmp/test27
 
-cd ${prefix}test27
+cd /usr/tmp/est27
 source bin/activate
 
 pip install pandas
-
