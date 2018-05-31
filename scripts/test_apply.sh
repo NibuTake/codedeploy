64cfc311974a5a3d0bc00ce39a17be03e4a1e5f8
morinibu <takeshi.morinibu@daikin.co.jp>
2018-05-31 11:28:20 +0900

    h

--- scripts/test_apply.sh
+++ scripts/test_apply.sh
@@ -2,5 +2,18 @@
 ifconfig
 cat /etc/passwd|sed -e 's/:.*//g'
 prefix='/home/ec2-user/'
-sudo /usr/local/bin/pip3.6 install virtualenv
+
+sudo yum -y groupinstall 'Development tools'
+sudo yum -y install zlib-devel openssl-devel
+
+mkdir ${prefix}python3.6
+cd ${prefix}python3.6
+wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+tar zxvf Python-3.6.1.tgz
+cd ${prefix}/python3.6/Python-3.6.1
+./configure
+make
+sudo make install
+
+/usr/local/bin/pip3.6 install virtualenv
 /usr/local/bin/virtualenv -p /usr/local/bin//python3.6 /home/ec2-user/encryptfile
